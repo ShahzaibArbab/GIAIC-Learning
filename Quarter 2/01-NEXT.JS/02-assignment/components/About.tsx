ce88f22ea97ffe2556b2902cc3c27dcca5e7fa30
ShahzaibArbab <Kingkhanjumma@gmail.com>
2025-01-15 13:32:17 +0500

    first commit

---
+++ Quarter 2/01-NEXT.JS/02-assignment/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section>
+      <h2>About Us</h2>
+      <p>This is a simple Next.js application using typescript.</p>
+    </section>
+  );
+};
+
+export default About;
